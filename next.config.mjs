0f93e614c0198cb6b843a86b6bdcd47b0bac3645
Dhpla12 <aradhyahaldikar@gmail.com>
2026-03-09 02:14:23 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,20 +1,5 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  images: {
-    unoptimized: true,
-  },
-  webpack: (config, { isServer }) => {
-    if (isServer) {
-      config.ignoreWarnings = [
-        ...(config.ignoreWarnings || []),
-        { module: /node_modules\/@supabase/ },
-      ]
-    }
-    return config
-  },
+  const nextConfig = {
+  turbopack: {}
 }
 
 export default nextConfig
